a1827e0010af32767c8de7cf43329cdd10a7efa5
onahump <oswaldo@makingdevs.com>
2019-05-14 19:17:51 -0500

    Adding some instance variables to show into views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,12 @@
 class HomeController < ApplicationController
   def index
+    @title = "Hello everyone this is our home page"
   end
 
   def who_are_we
   end
 
   def contact
+    @title = "This is a contact page"
   end
 end
