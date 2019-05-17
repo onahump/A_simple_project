2cb098a1350e0ee6b2deaa28d0758311c282c2ed
onahump <oswaldo@makingdevs.com>
2019-05-17 12:34:55 -0500

    Adding rout with method post to contact

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index' 
   get 'home/who_are_we'
   get 'home/contact'
+  post 'home/contact'
 
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
